0888bf33a9cef064f1d1ada1a6fe0f7210061e37
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-04-08 21:11:02 +0200

    Add menu astro componennt

--- src/components/icons/BarsIcon.jsx
+++ src/components/icons/BarsIcon.jsx
@@ -1,6 +1,6 @@
 export default function BarsIcon() {
   return (
-    <div className='p-1.5 bg-orange-300 rounded'>
+    <div className='p-1.5 bg-orange-300 rounded' id='toggle-menu'>
       <img class='w-6 h-6 inline-block' src='/bars-3.svg' alt='bars' />
     </div>
   );

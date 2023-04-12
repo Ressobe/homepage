bafb6a50249b96be520e7e9a1a0006097034ecac
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-04-12 09:21:40 +0200

    Add info about my skills

--- src/components/icons/BarsIcon.jsx
+++ src/components/icons/BarsIcon.jsx
@@ -1,7 +1,7 @@
 export default function BarsIcon() {
   return (
-    <div className='p-1.5 bg-orange-300 rounded' id='toggle-menu'>
-      <img class='w-6 h-6 inline-block' src='/bars-3.svg' alt='bars' />
+    <div className="p-1.5 active:opacity-80 bg-orange-300 rounded" id="toggle-menu">
+      <img className="w-6 h-6 inline-block" src="/bars-3.svg" alt="bars" />
     </div>
   );
 }
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,107 @@
 export const SITE_TITLE = "Bartosz Sobina Home Page";
-export const HOMEPAGE_URL = "";
-export const SITE_DESCRIPTION = "";
-export const REPO_URL = "https://github.com/Ressobe/homepage";
-export const navLinks: { label: string; href: string }[] = [
+export const SITE_DESCRIPTION = "My homepage created using astro, react and tailwind css";
+export const GITHUB_URL = "https://github.com/Ressobe";
+export const REPO_URL = `${GITHUB_URL}/homepage`;
+export const DEVICONS_URL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/";
+
+export const NAV_LINKS: { label: string; href: string }[] = [
   { label: "Home", href: "/" },
   { label: "Works", href: "/works" },
   { label: "Posts", href: "/posts" },
 ];
+
+export const PROJECTS: {
+  href: string;
+  src: string;
+  projectTitle: string;
+  projectDescription: string;
+}[] = [
+  {
+    href: `${GITHUB_URL}/terminalSite`,
+    src: "/terminalSite.png",
+    projectTitle: "Terminal Style Website",
+    projectDescription:
+      "Most of the time I 'm spending inside terminal thats why I created this site",
+  },
+
+  {
+    href: `${GITHUB_URL}/terminalSite`,
+    src: "/terminalSite.png",
+    projectTitle: "Terminal Style Website",
+    projectDescription:
+      "Most of the time I 'm spending inside terminal thats why I created this site",
+  },
+  {
+    href: `${GITHUB_URL}/terminalSite`,
+    src: "/terminalSite.png",
+    projectTitle: "Terminal Style Website",
+    projectDescription:
+      "Most of the time I 'm spending inside terminal thats why I created this site",
+  },
+  {
+    href: `${GITHUB_URL}/terminalSite`,
+    src: "/terminalSite.png",
+    projectTitle: "Terminal Style Website",
+    projectDescription:
+      "Most of the time I 'm spending inside terminal thats why I created this site",
+  },
+];
+
+export const STACK: {
+  name: string;
+  src: string;
+  level: number;
+}[] = [
+  {
+    name: "html",
+    src: "/html5/html5-original.svg",
+    level: 3,
+  },
+
+  {
+    name: "css",
+    src: "/css3/css3-original.svg",
+    level: 3,
+  },
+
+  {
+    name: "javascript",
+    src: "javascript/javascript-original.svg",
+    level: 2,
+  },
+
+  {
+    name: "react",
+    src: "react/react-original.svg",
+    level: 2,
+  },
+
+  {
+    name: "python",
+    src: "python/python-original.svg",
+    level: 4,
+  },
+
+  {
+    name: "linux",
+    src: "linux/linux-original.svg",
+    level: 3,
+  },
+
+  {
+    name: "git",
+    src: "git/git-original.svg",
+    level: 2,
+  },
+
+  {
+    name: "typescript",
+    src: "typescript/typescript-original.svg",
+    level: 1,
+  },
+];
+
+export const TO_LEARN: {
+  name: string;
+  src: string;
+}[] = [{ name: "rust", src: "rust/rust-plain.svg" }];

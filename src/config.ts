76bc6a0c1b2df4b5d874a94e0abb09d96ef73ce2
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-04-13 09:51:13 +0200

    Add categories to skills

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE = "Bartosz Sobina Home Page";
 export const SITE_DESCRIPTION = "My homepage created using astro, react and tailwind css";
 export const GITHUB_URL = "https://github.com/Ressobe";
-export const REPO_URL = `${GITHUB_URL}/homepage`;
+export const YOUTUBE_URL = "https://www.youtube.com/channel/UCcWXlahMIEBY9WR11VVELuQ";
 export const DEVICONS_URL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/";
 
 export const NAV_LINKS: { label: string; href: string }[] = [
@@ -46,62 +46,77 @@ export const PROJECTS: {
       "Most of the time I 'm spending inside terminal thats why I created this site",
   },
 ];
+enum SkillsCategory {
+  "frontend",
+  "backend",
+  "tools",
+}
 
-export const STACK: {
+export const SKILLS: {
   name: string;
   src: string;
   level: number;
+  category: SkillsCategory;
 }[] = [
   {
-    name: "html",
+    name: "HTML",
     src: "/html5/html5-original.svg",
-    level: 3,
+    level: 5,
+    category: SkillsCategory.frontend,
   },
 
   {
-    name: "css",
+    name: "CSS",
     src: "/css3/css3-original.svg",
     level: 3,
+    category: SkillsCategory.frontend,
   },
 
   {
-    name: "javascript",
+    name: "Javascript",
     src: "javascript/javascript-original.svg",
     level: 2,
+    category: SkillsCategory.frontend,
   },
 
   {
-    name: "react",
+    name: "React",
     src: "react/react-original.svg",
     level: 2,
+    category: SkillsCategory.frontend,
   },
 
   {
-    name: "python",
+    name: "Python",
     src: "python/python-original.svg",
     level: 4,
+    category: SkillsCategory.backend,
   },
 
   {
-    name: "linux",
+    name: "Linux",
     src: "linux/linux-original.svg",
     level: 3,
+    category: SkillsCategory.tools,
   },
 
   {
-    name: "git",
+    name: "Git",
     src: "git/git-original.svg",
     level: 2,
+    category: SkillsCategory.tools,
   },
 
   {
-    name: "typescript",
+    name: "Typescript",
     src: "typescript/typescript-original.svg",
     level: 1,
+    category: SkillsCategory.tools,
   },
 ];
 
 export const TO_LEARN: {
   name: string;
   src: string;
-}[] = [{ name: "rust", src: "rust/rust-plain.svg" }];
+  category: SkillsCategory;
+}[] = [{ name: "rust", src: "rust/rust-plain.svg", category: SkillsCategory.backend }];

60ad9809522371fa9e4a74a4791c4e1a55b0cdea
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-04-14 08:54:09 +0200

    Added functionality to arrows

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,50 +1,61 @@
 import { useEffect, useState } from "react";
-import { SKILLS } from "../config";
+import { SKILLS, categories } from "../config";
 import SkillItem from "./SkillItem";
-
-const categories = ["frontend", "backend", "tools"];
 
 const filtrSkills = (currentCategory) => {
   return SKILLS.filter((item) => item.category === currentCategory);
 };
 
 export default function Skills() {
-  const [currentCategory, setCurrentCategory] = useState("frontend");
-  const [skillsFiltred, setSkillsFiltred] = useState(() => filtrSkills(currentCategory));
+  const [currentCategory, setCurrentCategory] = useState(0);
+  const [skillsFiltred, setSkillsFiltred] = useState(() =>
+    filtrSkills(categories[currentCategory])
+  );
 
-  const handleClick = () => {
-    let idx = categories.indexOf(currentCategory);
+  const handleClickArrowRight = () => {
+    let idx = currentCategory;
     if (idx === categories.length - 1) {
       idx = 0;
     } else {
       idx++;
     }
-    setCurrentCategory(categories[idx]);
+    setCurrentCategory(idx);
+  };
+
+  const handleClickArrowLeft = () => {
+    let idx = currentCategory;
+    if (idx === 0) {
+      idx = categories.length - 1;
+    } else {
+      idx--;
+    }
+    setCurrentCategory(idx);
   };
 
   useEffect(() => {
-    setSkillsFiltred(filtrSkills(currentCategory));
+    setSkillsFiltred(filtrSkills(categories[currentCategory]));
   }, [currentCategory]);
+
   return (
-    <div className="relative min-w-max min-h-max bg-orange-200 dark:bg-zinc-700  mt-10 text-center border-2 border-orange-300 dark:border-zinc-600 rounded">
+    <div className="relative min-w-max h-80 bg-orange-200 dark:bg-zinc-700  mt-10 text-center border-2 border-orange-300 dark:border-zinc-600 rounded">
       <button
         className="absolute left-0 cursor-pointer my-1 mx-4 hover:opacity-60 active:opacity-40"
-        onClick={handleClick}
+        onClick={() => handleClickArrowLeft()}
       >
-        <img className="w-12  " src="/left-long-solid.svg" />
+        <img className="w-12" src="/left-long-solid.svg" />
       </button>
 
       <button
         className="absolute right-0 cursor-pointer my-1 mx-4 hover:opacity-60 active:opacity-40"
-        onClick={handleClick}
+        onClick={() => handleClickArrowRight()}
       >
         <img className="w-12" src="/right-long-solid.svg" />
       </button>
 
       <div className="mt-10 flex-col justify-center items-center w-full h-full">
-        <h1 className="font-bold mt-5">🧰 {currentCategory.toUpperCase()} </h1>
+        <h1 className="font-bold mt-5">🧰 {categories[currentCategory].toUpperCase()} </h1>
         {skillsFiltred.map(({ name, src, level }) => (
-          <SkillItem name={name} src={src} level={level} />
+          <SkillItem key={name} name={name} src={src} level={level} />
         ))}
       </div>
     </div>

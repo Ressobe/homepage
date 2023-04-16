c3c5cc871ae86edcbbb30afe27266bee67d3f706
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-04-16 21:00:55 +0200

    feat: click in dots chnage to specific category

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,35 +2,35 @@ import { useEffect, useState } from "react";
 import { SKILLS, categories } from "../consts";
 import SkillItem from "./SkillItem";
 
+// Slide animation from left and right
+
 const filtrSkills = (currentCategory) => {
   return SKILLS.filter((item) => item.category === currentCategory);
 };
 
 export default function Skills() {
-  const [currentCategoryIdx, setCurrentCategory] = useState(0);
-  const [skillsFiltred, setSkillsFiltred] = useState(() =>
-    filtrSkills(categories[currentCategoryIdx])
-  );
+  const [categoryIdx, setCategoryIdx] = useState(0);
+  const [skillsFiltred, setSkillsFiltred] = useState(() => filtrSkills(categories[categoryIdx]));
 
   const handleClickArrowRight = () => {
-    const isLastSlide = currentCategoryIdx === categories.length - 1;
-    const newIdx = isLastSlide ? 0 : currentCategoryIdx + 1;
-    setCurrentCategory(newIdx);
+    const isLastCategory = categoryIdx === categories.length - 1;
+    const newIdx = isLastCategory ? 0 : categoryIdx + 1;
+    setCategoryIdx(newIdx);
   };
 
   const handleClickArrowLeft = () => {
-    const isFristSlide = currentCategoryIdx === 0;
-    const newIdx = isFristSlide ? categories.length - 1 : currentCategoryIdx - 1;
-    setCurrentCategory(newIdx);
+    const isFristCategory = categoryIdx === 0;
+    const newIdx = isFristCategory ? categories.length - 1 : categoryIdx - 1;
+    setCategoryIdx(newIdx);
   };
 
   useEffect(() => {
-    setSkillsFiltred(filtrSkills(categories[currentCategoryIdx]));
-  }, [currentCategoryIdx]);
+    setSkillsFiltred(filtrSkills(categories[categoryIdx]));
+  }, [categoryIdx]);
 
   return (
     <div className="relative min-w-max bg-orange-100 dark:bg-zinc-700   text-center border-2 border-orange-300 dark:border-zinc-600 rounded">
-      <h2 className="mt-3 text-xl font-bold">{categories[currentCategoryIdx].toUpperCase()}</h2>
+      <h2 className="mt-3 text-xl font-bold">{categories[categoryIdx].toUpperCase()}</h2>
       <div className="mt-3  grid  grid-cols-1 grid-rows-3 gap-3 md:grid-rows-3 md:grid-cols-2 max-h-min">
         {skillsFiltred.map(({ name, src, level }) => (
           <SkillItem key={name} name={name} src={src} level={level} />
@@ -45,18 +45,15 @@ export default function Skills() {
           <img className="w-8" src="/left-long-solid.svg" />
         </button>
 
-        {Array(categories.length)
-          .fill(1)
-          .map((_, i) => (
-            <div
-              key={i}
-              className={`mx-2 opacity-80 ease-in duration-150 inline-block w-4 h-4 rounded-full  ${
-                i === currentCategoryIdx
-                  ? "bg-zinc-900 dark:bg-zinc-400"
-                  : "bg-zinc-500  dark:bg-black"
-              }`}
-            />
-          ))}
+        {categories.map((_, i) => (
+          <div
+            key={i}
+            className={`mx-2 opacity-80 ease-in duration-150 inline-block w-4 h-4 rounded-full cursor-pointer ${
+              i === categoryIdx ? "bg-zinc-900 dark:bg-zinc-400" : "bg-zinc-500  dark:bg-black"
+            }`}
+            onClick={() => setCategoryIdx(i)}
+          />
+        ))}
         <button
           className="cursor-pointer mx-4 hover:opacity-60 active:opacity-40"
           onClick={() => handleClickArrowRight()}

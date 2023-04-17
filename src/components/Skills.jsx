97a9f0d800ff637339301d7180e4c2ca91990036
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-04-17 19:36:58 +0200

    test: working on style of skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -29,38 +29,42 @@ export default function Skills() {
   }, [categoryIdx]);
 
   return (
-    <div className="relative min-w-max bg-orange-100 dark:bg-zinc-700 text-center dark:border-zinc-600 rounded">
-      <h2 className="mt-5 text-xl font-bold">{categories[categoryIdx].toUpperCase()}</h2>
-      <div className="mt-2 grid grid-cols-1 grid-rows-3 gap-4  md:grid-rows-3 md:grid-cols-2 max-h-min">
-        {skillsFiltred.map(({ name, src, level }) => (
-          <SkillItem key={name} name={name} src={src} level={level} />
-        ))}
+    <>
+      <div className="relative  bg-orange-100 dark:bg-zinc-800 text-center  rounded">
+        <h2 className="mt-5 text-xl font-bold">{categories[categoryIdx].toUpperCase()}</h2>
+        <div className="mt-2 grid grid-cols-1 grid-rows-3 gap-2  md:grid-rows-2 md:grid-cols-2 gap-y-6 max-h-fit">
+          {skillsFiltred.map(({ name, src, level }) => (
+            <SkillItem key={name} name={name} src={src} level={level} />
+          ))}
+        </div>
+
+        {/* <div className="flex justify-center items-center"> */}
+        {/*   <button */}
+        {/*     className="cursor-pointer mx-4 hover:opacity-60 active:opacity-40" */}
+        {/*     onClick={() => handleClickArrowLeft()} */}
+        {/*   > */}
+        {/*     <img className="w-8" src="/left-long-solid.svg" /> */}
+        {/*   </button> */}
+
+        {/*   <button */}
+        {/*     className="cursor-pointer mx-4 hover:opacity-60 active:opacity-40" */}
+        {/*     onClick={() => handleClickArrowRight()} */}
+        {/*   > */}
+        {/*     <img className="w-8" src="/right-long-solid.svg" /> */}
+        {/*   </button> */}
+        {/* </div> */}
       </div>
-
-      <div className="flex justify-center items-center">
-        <button
-          className="cursor-pointer mx-4 hover:opacity-60 active:opacity-40"
-          onClick={() => handleClickArrowLeft()}
-        >
-          <img className="w-8" src="/left-long-solid.svg" />
-        </button>
-
+      <div className="text-center mt-5">
         {categories.map((_, i) => (
           <div
             key={i}
-            className={`mx-2 opacity-80 ease-in duration-150 inline-block w-4 h-4 rounded-full cursor-pointer ${
+            className={`mx-2 opacity-80 ease-in duration-150 inline-block w-6 h-6 rounded-full cursor-pointer ${
               i === categoryIdx ? "bg-zinc-900 dark:bg-zinc-400" : "bg-zinc-500  dark:bg-black"
             }`}
             onClick={() => setCategoryIdx(i)}
           />
         ))}
-        <button
-          className="cursor-pointer mx-4 hover:opacity-60 active:opacity-40"
-          onClick={() => handleClickArrowRight()}
-        >
-          <img className="w-8" src="/right-long-solid.svg" />
-        </button>
       </div>
-    </div>
+    </>
   );
 }

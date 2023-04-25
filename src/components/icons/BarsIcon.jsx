5ebfe123b62db1bb30cd23f3c48eb8572316b28b
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-04-25 07:57:19 +0200

    fix: fix hamburger menu and change style for icon

--- src/components/icons/BarsIcon.jsx
+++ src/components/icons/BarsIcon.jsx
@@ -1,6 +1,9 @@
 export default function BarsIcon() {
   return (
-    <div className="p-1.5 active:opacity-80 bg-orange-300 rounded" id="toggle-menu">
+    <div
+      className="p-1.5 active:opacity-80 bg-orange-300 dark:bg-zinc-300 rounded"
+      id="toggle-menu"
+    >
       <img className="w-6 h-6 inline-block" src="/bars-3.svg" alt="bars" />
     </div>
   );

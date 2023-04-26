8120f8785d899d26a0753c43e7a37566f7c932bf
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-04-26 19:11:38 +0200

    fix: remove projects info because project will be created using md files

--- src/consts.ts
+++ src/consts.ts
@@ -6,119 +6,67 @@ export const DEVICONS_URL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/
 
 export const NAV_LINKS: { label: string; href: string }[] = [
   { label: "Home", href: "/" },
-  { label: "Works", href: "/works" },
+  { label: "Projects", href: "/projects" },
   { label: "Posts", href: "/posts" },
+  { label: "Workflow", href: "/workflow" },
 ];
 
-export const PROJECTS: {
-  href: string;
-  src: string;
-  projectTitle: string;
-  projectDescription: string;
-}[] = [
-  {
-    href: `${GITHUB_URL}/terminalSite`,
-    src: "/terminalSite.png",
-    projectTitle: "Terminal Style Website",
-    projectDescription:
-      "Most of the time I 'm spending inside terminal thats why I created this site",
-  },
-
-  {
-    href: `${GITHUB_URL}/terminalSite`,
-    src: "/terminalSite.png",
-    projectTitle: "Terminal Style Website",
-    projectDescription:
-      "Most of the time I 'm spending inside terminal thats why I created this site",
-  },
-  {
-    href: `${GITHUB_URL}/terminalSite`,
-    src: "/terminalSite.png",
-    projectTitle: "Terminal Style Website",
-    projectDescription:
-      "Most of the time I 'm spending inside terminal thats why I created this site",
-  },
-  {
-    href: `${GITHUB_URL}/terminalSite`,
-    src: "/terminalSite.png",
-    projectTitle: "Terminal Style Website",
-    projectDescription:
-      "Most of the time I 'm spending inside terminal thats why I created this site",
-  },
-];
-enum SkillsCategory {
-  "frontend",
-  "backend",
-  "tools",
-}
-
-export const categories = ["frontend", "backend", "tools"];
 
 export const SKILLS: {
   name: string;
   src: string;
   level: number;
-  category: string;
 }[] = [
   {
     name: "HTML",
     src: "/html5/html5-original.svg",
     level: 5,
-    category: "frontend",
   },
 
   {
     name: "CSS",
     src: "/css3/css3-original.svg",
     level: 3,
-    category: "frontend",
   },
 
   {
     name: "Javascript",
     src: "javascript/javascript-original.svg",
     level: 2,
-    category: "frontend",
   },
 
   {
     name: "React",
     src: "react/react-original.svg",
     level: 2,
-    category: "frontend",
   },
 
   {
     name: "Python",
     src: "python/python-original.svg",
     level: 4,
-    category: "backend",
   },
 
   {
     name: "Linux",
     src: "linux/linux-original.svg",
     level: 3,
-    category: "tools",
   },
 
   {
     name: "Git",
     src: "git/git-original.svg",
     level: 2,
-    category: "tools",
   },
 
   {
     name: "Typescript",
     src: "typescript/typescript-original.svg",
     level: 1,
-    category: "tools",
   },
 ];
 
 export const TO_LEARN: {
   name: string;
   src: string;
-  category: SkillsCategory;
-}[] = [{ name: "rust", src: "rust/rust-plain.svg", category: SkillsCategory.backend }];
+}[] = [{ name: "rust", src: "rust/rust-plain.svg" }];
